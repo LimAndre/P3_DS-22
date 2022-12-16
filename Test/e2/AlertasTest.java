9c283fb88df0c0945f8b205f7c9ea47791a2d5b8
LimAndre <andre.lima@udc.es>
2022-12-16 20:14:11 +0100

    Acabado

--- Test/e2/AlertasTest.java
+++ Test/e2/AlertasTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class AlertasTest {
 
+    //FOCAS
     Tanque tanqueFocas= new Tanque("Piscina de las Focas", "Exterior");
 
     DispositivosControl controlTemperaturaFocas= new DispositivosControl(15);
@@ -21,9 +22,26 @@ public class AlertasTest {
     Personal personalFocas= new Personal("Alertas de mantenimiento Focas");
 
 
+    //PINGUINOS
+    Tanque tanquePinguinos= new Tanque("Piscina de los Pinguinos", "Interior");
+    DispositivosControl controlTemperaturaPinguinos= new DispositivosControl(10);
+    DispositivosControl controlPHPinguinos= new DispositivosControl(50);
+
+    Sensor sensorTemperaturaPinguinos= new Sensor("Temperatura", controlTemperaturaPinguinos, tanquePinguinos);
+    Sensor sensorPHPinguinos= new Sensor("PH",controlPHPinguinos, tanquePinguinos);
+
+    Alerta alertaRojaTemperaturaPinguinos= new Alerta("Control de Temperatura Pinguinos",20, -20, Alerta.TipoAlarma.Roja);
+    Alerta alertaNaranjaTemperaturaPinguinos= new Alerta("Control de Temperatura Focas",15, -10, Alerta.TipoAlarma.Naranja);
+
+    Alerta alertaRojaPHPinguinos= new Alerta("Control de PH Pinguinos",200, -100, Alerta.TipoAlarma.Roja );
+    Alerta alertaNaranjaPHPinguinos= new Alerta("Control de PH Pinguinos",100, 0, Alerta.TipoAlarma.Naranja);
+
+    Personal personalPinguinos= new Personal("Alertas de mantenimiento Pinguinos");
 
     @Test
     void testing(){
+
+        //TestingFocas
         alertaRojaTemperaturaFocas.añadirSuscriptores(personalFocas);
         alertaNaranjaTemperaturaFocas.añadirSuscriptores(personalFocas);
 
@@ -39,6 +57,24 @@ public class AlertasTest {
         sensorTemperaturaFocas.setNivel(50);
         sensorPHFocas.setNivel(5000);
         personalFocas.imprimirInformes();
+
+        //TestingPinguinos
+        alertaRojaTemperaturaPinguinos.añadirSuscriptores(personalPinguinos);
+        alertaNaranjaTemperaturaPinguinos.añadirSuscriptores(personalPinguinos);
+
+        alertaNaranjaPHPinguinos.añadirSuscriptores(personalPinguinos);
+        alertaRojaPHPinguinos.añadirSuscriptores(personalPinguinos);
+
+        sensorTemperaturaPinguinos.attach(alertaRojaTemperaturaPinguinos);
+        sensorTemperaturaPinguinos.attach(alertaNaranjaTemperaturaPinguinos);
+
+        sensorPHPinguinos.attach(alertaNaranjaPHPinguinos);
+        sensorPHPinguinos.attach(alertaRojaPHPinguinos);
+
+        sensorTemperaturaPinguinos.setNivel(30);
+        sensorPHPinguinos.setNivel(55);
+        personalPinguinos.imprimirInformes();
+
 
     }
 
